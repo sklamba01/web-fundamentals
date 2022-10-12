6b01c017e288eabd31d270aecabf11501d662707
Sohil lamba <sohil.lamba@getfareye.com>
2022-10-12 16:49:29 +0530

    Completed Today's assignment

--- day1/api/index.js
+++ day1/api/index.js
@@ -1,16 +1,34 @@
 // How to fetch Data from server.
 
 function fetchDataFromServer() {
-  fetch("https://jsonplaceholder.typicode.com/todos")
+  fetch("https://dummyjson.com/users")
     .then((response) => response.json())
     .then((json) => printData(json));
 }
 
 function printData(data) {
   console.log(data);
-  let ele = document.getElementById("code");
-  let tempData = data.filter((a) => a.id < 50);
-  ele.innerHTML = JSON.stringify(tempData, null, 4);
+  let {limit, skip, total, users} = data;
+  console.log(users);
+  let age_avg = users.reduce((a, b) => {
+    a += b.age;
+    return a;
+  }, 0);
+  console.log(age_avg / users.length);
+
+  let all = users.reduce((a, b) => {
+    a.push({name:b.firstName + " " + b.lastName, age:b.age, weight:b.weight});
+    return a;
+  }, []);
+  console.log(all);
+  // all = all.filter((e) => e.age > 50);
+  // console.log(all);
+
+  let avg_weight = all.reduce((a, b) => {
+    a += b.weight;
+    return a;
+  }, 0);
+  console.log(avg_weight / all.length);
 }
 
 fetchDataFromServer();
---
+++ day1/starter/index.js
@@ -0,0 +1,70 @@
+
+
+const nameFormatter = (user) => `${user.firstName} ${user.lastName}`;
+const companyFormatter = (user) => `${user.company.name}`;
+const positionFormatter = (user) => `${user.company.title}`;
+
+let config = [
+    {key: "name", label: "Name", formatter: nameFormatter},
+    {key: "age", label: "Age"},
+    {key: "phone", label: "Phone"},
+    {key: "bloodGroup", label: "Blood group"},
+    {key: "company", label: "Company", formatter: companyFormatter},
+    {key: "position", label: "Position", formatter: positionFormatter},
+    {key: "weight", label: "Weight"},
+];
+
+function renderUserListing() {
+    fetch("https://dummyjson.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        try {
+            let main = document.getElementsByClassName("main-conatiner")[0];
+            json.users.forEach((user) => {
+                main.appendChild(showUser(user));
+            });
+        } catch(ex) {
+            console.log(ex);
+        }
+      });
+}
+
+
+function showUser(user) {
+    let cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "card");
+    let profileImage = document.createElement("img");
+    profileImage.setAttribute("class", "profile-image");
+    profileImage.setAttribute("src", user.image);
+    cardDiv.appendChild(profileImage);
+    // cardDiv.appendChild(document.createElement("br"));
+    let table = document.createElement("table");
+    config.forEach((field) => {
+        let value;
+        if (field.formatter) {
+            value = field.formatter(user);
+        } else {
+            value = user[field.key];
+        }
+        table.appendChild(createRow(field.label, value));
+    });
+    console.log(table);
+    cardDiv.appendChild(table);
+    return cardDiv;
+}
+
+function createRow(label, value) {
+    let row = document.createElement("tr");
+    row.appendChild(createCol(label, true));
+    row.appendChild(createCol(value, false));
+    return row;
+}
+
+function createCol (data, isHeading) {
+    let col = document.createElement("td");
+    col.setAttribute("class", isHeading ? "heading" : "value");
+    col.innerHTML = data;
+    return col;
+}
+
+renderUserListing()
